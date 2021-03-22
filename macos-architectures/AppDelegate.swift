35ebafa7ea9c7fb43a745512e415a580fe9c380b
Tony Zhuang <tony.zhuang@qileap.com>
2021-03-22 12:28:46 -0700

    Initial Commit

---
+++ macos-architectures/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos-architectures
+//
+//  Created by Xinyi Zhuang on 2021-03-22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos-architectures/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macos-architectures
+//
+//  Created by Xinyi Zhuang on 2021-03-22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
