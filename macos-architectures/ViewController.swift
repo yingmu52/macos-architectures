724843d9f471530042a9bc3fdd9fefe762296771
Tony Zhuang <tony.zhuang@qileap.com>
2021-03-22 20:16:21 -0700

    mvc: refractor + store data

--- macos-architectures/ViewController.swift
+++ macos-architectures/ViewController.swift
@@ -7,16 +7,28 @@
 
 import Cocoa
 
-class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+class ViewController: NSViewController {
     
-    @IBOutlet weak var tableView: NSTableView!
-    @IBOutlet weak var inputTextField: NSTextField!
+    @IBOutlet weak private var tableView: NSTableView!
+    @IBOutlet weak private var inputTextField: NSTextField!
     
-    var todoItems = [String]() {
+    private(set) var todoItems = [String]() {
         didSet {
-            self.view.window?.title = "\(todoItems.count) todo items"
-            self.tableView.reloadData()
+            setWindowTitle()
+            tableView.reloadData()
+            UserDefaults.standard.set(todoItems, forKey: "TodoItems")
         }
+    }
+    
+    private(set) var completedItems = [String]() {
+        didSet {
+            setWindowTitle()
+            tableView.reloadData()
+        }
+    }
+    
+    func setWindowTitle() {
+        view.window?.title = "\(todoItems.count) todo items \(completedItems.count) completed"
     }
     
     override func viewDidLoad() {
@@ -39,12 +51,28 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         tableView.dataSource = self
         tableView.delegate = self
         inputTextField.delegate = self
+        
+        readData()
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        setWindowTitle()
+    }
+    
+    func readData() {
+        if let items = UserDefaults.standard.value(forKey: "TodoItems") as? [String] {
+            todoItems = items
+        }
     }
     
     @objc func doubleClick() {
-        todoItems.remove(at: tableView.clickedRow)
+        let removedItem = todoItems.remove(at: tableView.clickedRow)
+        completedItems.append(removedItem)
     }
-    
+}
+
+extension ViewController: NSTableViewDelegate, NSTableViewDataSource {
     func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
         return false
     }
@@ -64,9 +92,9 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         cell.wantsLayer = true
         cell.layer?.backgroundColor = NSColor.systemIndigo.cgColor
         cell.layer?.cornerRadius = 5
+        cell.textField?.textColor = .white
         
         cell.textField?.stringValue = todoItems[row]
-        cell.textField?.textColor = .white
         return cell
     }
 }
@@ -74,11 +102,10 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
 extension ViewController: NSTextFieldDelegate {
     func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
         // Do something against ENTER key
-        guard commandSelector == #selector(NSResponder.insertNewline) else { return false }
-        if !inputTextField.stringValue.isEmpty {
-            todoItems.append(inputTextField.stringValue)
-            inputTextField.stringValue.removeAll()
-        }
+        guard commandSelector == #selector(NSResponder.insertNewline), !inputTextField.stringValue.isEmpty else { return false }
+        
+        todoItems.append(inputTextField.stringValue)
+        inputTextField.stringValue.removeAll()
         return true
     }
 }
