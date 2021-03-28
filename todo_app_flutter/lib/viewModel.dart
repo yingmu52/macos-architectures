a0b898ed1df15aa9e37606875d07ef6f452664fd
Xinyi <yingmu52@msn.com>
2021-03-28 15:13:02 -0700

    finished flutter todo app

--- todo_app_flutter/lib/viewModel.dart
+++ todo_app_flutter/lib/viewModel.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
 import 'package:todo_app_flutter/model.dart';
 
 class ViewModel {
   List<String> _todoItems = [];
   List<String> _completedItems = [];
+  final LocalStorage storage = LocalStorage('todo storage');
 
-  ViewModel();
+  ViewModel() {
+    _todoItems = storage.getItem('TodoItems') ?? [];
+    _completedItems = storage.getItem('CompletedItems') ?? [];
+  }
 
   List<Model> get items {
     return [
@@ -17,12 +22,22 @@ class ViewModel {
   }
 
   int get length {
-    return _todoItems.length + _completedItems.length;
+    var count = 0;
+    if (_todoItems != null) {
+      count += _todoItems.length;
+    }
+
+    if (_completedItems != null) {
+      count += _completedItems.length;
+    }
+    return count;
+    return (_todoItems.length) + (_completedItems.length ?? 0);
   }
 
   void addTodo(String item) {
     if (item.isNotEmpty) {
       _todoItems.add(item);
+      storage.setItem("todoItems", _todoItems);
     }
   }
 
@@ -32,9 +47,12 @@ class ViewModel {
       case TodoType.todo:
         var removedTodo = _todoItems.removeAt(index);
         _completedItems.insert(0, removedTodo);
+        storage.setItem("TodoItems", _todoItems);
+        storage.setItem("CompletedItems", _completedItems);
         break;
       case TodoType.completed:
         _completedItems.removeAt(index - _todoItems.length);
+        storage.setItem("CompletedItems", _completedItems);
         break;
     }
   }
