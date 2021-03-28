d3c462c03eee26f16856b124fb459fc174d70657
Xinyi <yingmu52@msn.com>
2021-03-28 13:21:11 -0700

    flutter: add mvvm

--- todo_app_flutter/lib/main.dart
+++ todo_app_flutter/lib/main.dart
@@ -9,105 +9,142 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodoListView(viewModel: ViewModel()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoListView extends StatefulWidget {
+  TodoListView({Key key, this.viewModel}) : super(key: key);
+  final ViewModel viewModel;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TodoListViewState createState() => _TodoListViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoListViewState extends State<TodoListView> {
+  List<Widget> get listChildren {
+    return widget.viewModel.items.map<GestureDetector>((item) {
+      var c = Container(
+        decoration: BoxDecoration(
+          color: item.type == TodoType.todo ? Colors.indigo : Colors.grey,
+          borderRadius: BorderRadius.all(
+            Radius.circular(5),
+          ),
+        ),
+        margin: EdgeInsets.only(top: 8, left: 8, right: 8),
+        child: Container(
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              item.content,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              maxLines: 2,
+            ),
+          ),
+          margin: EdgeInsets.all(item.type == TodoType.todo ? 12 : 10),
+        ),
+      );
+      return GestureDetector(
+        child: c,
+        onDoubleTap: () {
+          print("${item.content} tapped");
+        },
+      );
+    }).toList();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Container get inputTextView {
+    return Container(
+      color: Colors.grey,
+      padding: EdgeInsets.all(8),
+      height: 60,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(5),
+          ),
+        ),
+        padding: EdgeInsets.all(8),
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+                border: InputBorder.none, labelText: 'Enter your username'),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: listChildren,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Divider(),
+          inputTextView
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+enum TodoType {
+  todo,
+  completed,
+}
+
+class Model {
+  final TodoType type;
+  final String content;
+
+  Model({this.type, this.content});
+}
+
+class ViewModel {
+  List<String> _todoItems = [
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf12312312312312312",
+    // "aadfadfadfafadfadfadfafadfadfadfafadfadfadfafadfadfadfaf",
+    "2",
+    "3",
+  ];
+  List<String> _completedItems = ["4", "5", "6"];
+
+  List<Model> get items {
+    return [
+      ..._todoItems
+          .map((string) => Model(type: TodoType.todo, content: string)),
+      ..._completedItems
+          .map((string) => Model(type: TodoType.completed, content: string)),
+    ];
+  }
+
+  ViewModel();
+
+  void addTodo(String item) {
+    _todoItems.add(item);
   }
 }
